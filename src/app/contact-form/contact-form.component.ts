86ddb3b73de92f0732142f08b843553b36d0ae8f
Norbi <galgoczinorbi@gmail.com>
2022-02-13 19:03:08 +0100

    add contact form & footer

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
 
+  myform = new FormGroup({
+    name: new FormControl('', [Validators.required,]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    services: new FormControl(),
+    message:new FormControl(''),
+    dataProtection: new FormControl(false, [Validators.requiredTrue]),
+  });
+
+  servicesList = ['Logó, arculat tervezés', 'Webdesign', 'Webfejlesztés', 'Webshop készítés'];
+
+  checked = false;
+
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  onSubmit() { 
+    console.log(this.checked);
+    console.log(this.myform);
+   }
+
+  getEmailErrorMessage() {
+    if (this.myform.controls['email'].hasError('required')) {
+      return 'Kérlek add meg az email címed';
+    }
+
+    return this.myform.controls['email'].hasError('email') ? 'Nem érvényes email cím' : '';
+  }
+
+  getNameErrorMessage() {
+    return this.myform.controls['email'].hasError('required') ? 'Kérlek add meg a neved' : '';
+  }
+
+  getServicesErrorMessage() {
+    return this.myform.controls['services'].hasError('required') ? 'Kérlek válassz legalább egy lehetőséget' : '';
   }
 
 }
